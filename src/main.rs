1fdd8ee964a34df132cdf149561d7a3b69ebed42
Jockey12 <jockey101@proton.me>
2026-02-26 14:00:29 -0500

    working gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-fn multiply_matrix(_a: &[i32]) {
-    let a: [i32; 24] = [0; 24];
-
-    for x in a {
-        print!("{x} ");
+use std::io;
+fn multiply_matrix(a: &[i32], b: &[i32]) -> [i32; 8] {
+    let mut result = [0; 8];
+    for i in 0..a.len() {
+        for j in 0..b.len() {
+            result[i * b.len() + j] += a[i] * b[j];
+        }
     }
+    result
 }
 
 fn main() {
     println!("calculator");
-    let a: [i32; 8] = [0; 8];
-    multiply_matrix(&a);
+    let mut a: [i32; 8] = [0; 8];
+    for i in 0..a.len() {
+        println!("enter a value {};", i + 1);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("failed");
+        a[i] = input.trim().parse().expect("not a num");
+    }
+    let b: [i32; 8] = [2, 4, 6, 8, 10, 12, 14, 16];
+    let result = multiply_matrix(&a, &b);
+    println!("\n user array");
+    for x in a {
+        print!("{x} ");
+    }
+    println!("\n new array");
+    for x in result {
+        print!("{x} ");
+    }
 }
