b0f1041099cf8606b2bc43c6b54caf33e019bffc
Jockey12 <jockey101@proton.me>
2026-02-26 14:40:32 -0500

    multiply arrays

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::io;
 fn multiply_matrix(a: &[i32], b: &[i32]) -> [i32; 8] {
     let mut result = [0; 8];
-    for i in 0..a.len() {
-        for j in 0..b.len() {
-            result[i * b.len() + j] += a[i] * b[j];
-        }
+    let n = a.len().min(b.len()).min(result.len());
+    for i in 0..n {
+        result[i] = a[i] * b[i];
     }
     result
 }
@@ -12,16 +11,26 @@ fn multiply_matrix(a: &[i32], b: &[i32]) -> [i32; 8] {
 fn main() {
     println!("calculator");
     let mut a: [i32; 8] = [0; 8];
+    let mut b: [i32; 8] = [0; 8];
     for i in 0..a.len() {
         println!("enter a value {};", i + 1);
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("failed");
         a[i] = input.trim().parse().expect("not a num");
     }
-    let b: [i32; 8] = [2, 4, 6, 8, 10, 12, 14, 16];
+    for i in 0..b.len() {
+        println!("enter a value {};", i + 1);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("failed");
+        b[i] = input.trim().parse().expect("not a num");
+    }
     let result = multiply_matrix(&a, &b);
-    println!("\n user array");
+    println!("\n  array A");
     for x in a {
+        print!("{x} ");
+    }
+    println!("\n array B");
+    for x in b {
         print!("{x} ");
     }
     println!("\n new array");
